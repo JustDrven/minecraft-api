23629fec6314463547988b505e71bca9005ef61a
JustDrven <petr.nemec11111@gmail.com>
2025-01-17 22:42:16 +0100

    Updated verify of API Key & added ETag into list controller

--- src/main/java/dev/justdrven/minecraft/user/controller/UserController.java
+++ src/main/java/dev/justdrven/minecraft/user/controller/UserController.java
@@ -5,13 +5,16 @@ import dev.justdrven.minecraft.user.exception.BadRequestBodyException;
 import dev.justdrven.minecraft.user.exception.UserAlreadyExistException;
 import dev.justdrven.minecraft.user.exception.UserNotFoundException;
 import dev.justdrven.minecraft.user.orm.User;
-import dev.justdrven.minecraft.user.orm.UserRequest;
+import dev.justdrven.minecraft.user.request.UserRequest;
 import dev.justdrven.minecraft.user.repository.UserRepository;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -29,27 +32,34 @@ public class UserController {
 
     @GetMapping("/uuid/{uuid}")
     public ResponseEntity<User> getUserByUUID(@PathVariable("uuid") String uuid) {
-        return ResponseEntity.ok(userRepository.findByUuid(uuid).orElseThrow(UserNotFoundException::new));
+        return ResponseEntity.ok(userRepository.findByUuid(uuid)
+                .orElseThrow(UserNotFoundException::new));
 
     }
 
     @GetMapping("/list")
-    public List<User> getUsers(@RequestHeader(value = "X-API-Key", required = false) String key) {
-        if (!checkAPIKey(key)) {
+    public ResponseEntity<List<User>> getUsers(@RequestHeader(value = "X-API-Key", required = false) String key) {
+        if (!verifyAPIKey(key)) {
             throw new AccessDeniedException();
         }
 
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ETag", Base64.getEncoder().encodeToString(users.toString().getBytes()));
+
+        return new ResponseEntity<>(users, headers, HttpStatus.OK);
     }
 
     @GetMapping("/nick/{nick}")
     public ResponseEntity<User> getUserByNick(@PathVariable("nick") String nick) {
-        return ResponseEntity.ok(userRepository.findByNick(nick).orElseThrow(UserNotFoundException::new));
+        return ResponseEntity.ok(userRepository.findByNick(nick)
+                .orElseThrow(UserNotFoundException::new));
     }
 
     @PostMapping
     public ResponseEntity<User> addUser(@RequestHeader(value = "X-API-Key", required = false) String key, @RequestBody(required = false) UserRequest userRequest) {
-        if (!checkAPIKey(key)) {
+        if (!verifyAPIKey(key)) {
             throw new AccessDeniedException();
         }
 
@@ -70,8 +80,12 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
-    private boolean checkAPIKey(String key) {
-        return key != null && System.getProperty("API_KEY", "test").equals(key);
+    private boolean verifyAPIKey(String key) {
+        if (key == null) {
+            return false;
+        }
+
+        return System.getProperty("API_KEY", "test").equals(key);
     }
 
 }
